3417c36a6b94632f3a2456d350ff0b7fb7932129
nai golf <niy_golf@hotmail.com>
2019-03-14 12:37:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var app = express();
 
+const books = require('./db')
+
+const bodyParser = require('body-parser')
+
 var port = process.env.PORT || 7777;
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 app.get('/', function (req, res) {
     res.send('maker-diy');
 });
-
 
 
 app.get('/:key/:ch/:value', function (req, res) {
@@ -18,6 +25,40 @@ var msg = {"channel":ch,"value":value}
     res.send(msg);
 });
 
+//////////////////////////////////////////////////////////////
+app.get('/books', (req, res) => {
+  res.json(books)
+})
+
+app.get('/books/:id', (req, res) => {
+  res.json(books.find(book => book.id === req.params.id))
+})
+
+app.post('/books', (req, res) => {
+  books.push(req.body)
+  res.status(201).json(req.body)
+})
+
+
+app.put('/books/:id', (req, res) => {
+  const updateIndex = books.findIndex(book => book.id === req.params.id)
+  res.json(Object.assign(books[updateIndex], req.body))
+})
+
+app.delete('/books/:id', (req, res) => {
+   const deletedIndex = books.findIndex(book => book.id === req.params.id)
+   books.splice(deletedIndex, 1)
+   res.status(204).send()
+})
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////
 app.listen(port, function() {
     console.log('Starting node.js on port ' + port);
 });
